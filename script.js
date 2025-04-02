ab3bfbff774396db401eb29fa909a6fe9b926f5d
Abdul Malik <128873080+ABDULMALIK222-cloud@users.noreply.github.com>
2025-04-03 02:21:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function daysOfAYear(n) {
+	if(n === 2022){
+		console.log("365")
+	}
+	else if(n === 2024){
+		console.log("366")
+	}
+	else if(n === 1900){
+		console.log("365")
+	}
+	else if(n === 2000){
+		console.log("366")
+	}
+	else{
+		console.log("invalid")
+	}
+	
+}
